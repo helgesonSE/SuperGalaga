784dbbe946c7a5f25a010a721b82f344daf4f37a
Oskar Laulajainen <oskar.laulajainen.syne23lin@edu.tucsweden.se>
2024-04-05 15:12:04 +0200

    MULTIPLE WAVES IN A WAVE!!!!!

--- SuperGalaga/Assets/Scripts/WaveTemp.cs
+++ SuperGalaga/Assets/Scripts/WaveTemp.cs
@@ -3,28 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class SubWave
+{
+    public Enemy[] enemies;
+    public MotionType[] motionTypes;
+    public float[] speeds;
+    public float[] movementSpeed;
+    public float[] movementRadius;
+    public GameObject spawnPoint;
+    public float enemyVerticalSpacing = 1.0f;
+    public float enemyHorizontalSpacing = 1.0f;
+    public float timeToNextSubWave;
+    public float timeToNextEnemy;
+
+}
+
 public class WaveTemp : MonoBehaviour
 {
     [SerializeField]
-    public Enemy[] enemies;
-    [SerializeField]
-    public MotionType[] motionTypes;
-    [SerializeField]
-    public float[] speeds;
-    [SerializeField]
-    public float[] movementSpeed;
-    [SerializeField]
-    public float[] movementRadius;
-    [SerializeField]
-    public float timeToNextEnemy;
+    public SubWave[] subWaves;
     [SerializeField]
     public float timeToNextWave;
-    [SerializeField]
-    public GameObject spawnPoint;
-    [SerializeField]
-    public float enemyVerticalSpacing = 1.0f;
-    [SerializeField]
-    public float enemyHorizontalSpacing = 1.0f;
     [HideInInspector] public int enemiesLeft;
-
 }
