29fa542dcb9a4fd82e968919bdc2efcdf8dd4d8c
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2024-04-05 13:48:20 +0200

    Resolved merge conflict by using my version of TagManager.asset

---
+++ SuperGalaga/Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public GameObject Projectile12;
+    public float fireRate = 1f; // Tid i sekunder mellan skott
+
+    private float nextFireTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Kontrollera om det är dags att skjuta igen
+        if (Time.time >= nextFireTime)
+        {
+            //fixa det
+            // Skjut och uppdatera nästa gång att skjuta
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        // Skapa ett skott vid spawnerns position
+        Instantiate(Projectile12, transform.position, Quaternion.identity);
+    }
+}
