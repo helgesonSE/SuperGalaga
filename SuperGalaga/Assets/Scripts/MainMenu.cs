3ae2a06bed4b8f691962080b074721ba5cd4dc9a
SamNosratabadi <samnosratabadi@gmail.com>
2024-04-08 17:36:54 +0200

    added sound for extra life power up

--- SuperGalaga/Assets/Scripts/MainMenu.cs
+++ SuperGalaga/Assets/Scripts/MainMenu.cs
@@ -11,12 +11,7 @@ public class MainMenu : MonoBehaviour
         string levelName = "Level" + levelId;
         SceneManager.LoadScene(levelName);
     }
-    // Start is called before the first frame update
-    //public void StartGame()
-    //{
-    //    SceneManager.LoadScene("Level1");
-    //    SceneManager.LoadScene("Level2");
-    //}
+
     public void QuitGame()
     {
         Debug.Log("QUIT");
